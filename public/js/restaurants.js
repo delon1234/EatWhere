07614d3d6fc4f65bfbad1a15b7a09f4ddb6de93a
delon1234 <delonquok@gmail.com>
2021-01-17 19:51:07 +0800

    Some Code

--- public/js/restaurants.js
+++ public/js/restaurants.js
@@ -1,11 +1,33 @@
-const { response } = require("express");
+const Neighbourhood = require("../../models/Neighbourhood");
 
 function fetchNeighbourhoodsCuisinesCategories(){
     var request = new XMLHttpRequest();
     request.open("GET", restauranturl, true);
     request.onload = function(){
-        neighbourhood_array = JSON.parse(request.responseText);
+        console.log(request.responseText);
+        data = JSON.parse(request.responseText);
+        neighbourhoods = data[0];
+        categories = data[1];
+        cuisines = data[2];
     };
-    //get result[0], result[1], result[2]
+    
     request.send();
+}
+function getRestaurantsData(){
+    var request = new XMLHttpRequest();
+    request.open("GET", "/restaurants/all", true);
+    request.onload = function() {
+        restaurantsData = JSON.parse(request.responseText);
+        restaurants = restaurantsData[0];
+        reviewsData = restaurantsData[1];
+        console.log(restaurants[0]);
+        console.log(reviewsData);
+    };
+    request.send();
+}
+function displayRestaurants(){
+    totalRestaurants = restaurants.length;
+    for (var i = 0; i < totalRestaurants; i++){
+        var listing = "Insert RestaurantListing HTML";
+    }
 }
